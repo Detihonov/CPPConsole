86466ce1a74152d83ecee7ea5044cad682b25a5e
Denis Tikhonov <ndenmru@gmail.com>
2025-09-02 17:42:59 +0300

    feat: Add Project Unary Operators

---
+++ UnaryOperators/UnaryOperators/UnaryOperators.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int x{ 2 }, y{ 5 }, z{ 7 };
+
+    int temp = 0;
+
+    cout << "X = " << x << "\tY = " << y << "\tZ = " << z << endl;
+    cout << "++Z / X = " << ++z / x << endl;
+
+    cout << "\nX = " << x << "\tY = " << y << "\tZ = " << z << endl;
+    cout << "X * Y-- = " << x * y-- << endl;
+    
+    cout << "\nX = " << x << "\tY = " << y << "\tZ = " << z << endl;
+}
