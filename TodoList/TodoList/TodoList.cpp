aaf623449ad4ec050a8b1ed9505e79eb5e8bb076
Denis Tikhonov <ndenmru@gmail.com>
2025-09-02 15:05:52 +0300

    Add Project TodoList

---
+++ TodoList/TodoList/TodoList.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    //TODO: Do something
+
+    //TODO: Enter name from keyboard
+
+    //TODO: Check if Bob is in the list of names
+
+    //TODO: If Bob is in the list of names, print "BOOOOOOO!", else print "Yay".
+    
+    int u{}, q{ 11 }, z(2); // multiple declaration - Initialized
+    cout << u << endl; // 0
+    cout << q << endl; // 11
+    cout << z << endl; // 2
+}
